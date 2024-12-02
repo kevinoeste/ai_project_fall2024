10bc4b45e187598a0e4b1afe5633d4e3ed64fa0f
kevinoeste <62162997+kevinoeste@users.noreply.github.com>
2024-12-02 11:59:42 -0500

    Create neuralNet.py
    Setting up pytorch, need to do more later

---
+++ neuralNet.py
@@ -0,0 +1,31 @@
+import torch
+
+################## INITIALIZATION #######################
+
+#define directions
+#tensors that only have a single value are scalars
+forwards = 1
+backwards = -1
+
+#The four states that we need to control:
+#X position of cart
+#Velocity of cart
+#angle of pendulum
+#angular speed of pendulum
+
+#define default states
+cartPosition = 0
+cartVelocity = 0
+pendulumAngle = 0
+angularSpeed = 0
+
+#create tensor for inputs
+inputs = torch.tensor([[cartPosition, cartVelocity, pendulumAngle, angularSpeed]])
+
+
+################## REPRESENTATION #######################
+
+
+
+
+################## OPTIMIZATION #########################
