30a2c2ef90ddabe0f49f8a07b6540bd343ac6f2a
kevinoeste <62162997+kevinoeste@users.noreply.github.com>
2024-12-02 19:00:02 -0500

    Update neuralNet.py
    created cart neural network class

--- neuralNet.py
+++ neuralNet.py
@@ -21,12 +21,23 @@ cartVelocity = 0
 pendulumAngle = 0
 angularSpeed = 0
 
-#create tensor for inputs
-inputs = torch.tensor([[cartPosition, cartVelocity, pendulumAngle, angularSpeed]])
 
 
 ################## REPRESENTATION #######################
+#create tensor for inputs
+inputs = torch.tensor([[cartPosition, cartVelocity, pendulumAngle, angularSpeed]])
 
+class cartNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.flatten = nn.Flatten()
+    self.linear_relu_stack_x = nn.Sequential(
+      nn.Linear(100*101, 8192),
+      nn.ReLU(),
+      nn.linear(8192, 8192),
+      nn.ReLU(),
+      nn.Linear(8192, 1),
+    )
 
 
 
