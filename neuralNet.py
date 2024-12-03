31726a0b92d23e48f414d72fa53305c2b1a06390
kevinoeste <62162997+kevinoeste@users.noreply.github.com>
2024-12-02 20:22:32 -0500

    Update neuralNet.py
    Updating cartNN for the millionth time in a row

--- neuralNet.py
+++ neuralNet.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 ################## INITIALIZATION #######################
 
@@ -15,9 +16,10 @@ backwards = -1
 #angle of pendulum
 #angular speed of pendulum
 
-
-
-
+num_inputs = 4
+#this is an arbitrary number
+hidden_layer_size = 16
+num_outputs = 1
 
 ################## REPRESENTATION #######################
 #create tensor for inputs
@@ -26,19 +28,14 @@ inputs = torch.tensor([[cartPosition, cartVelocity, pendulumAngle, angularSpeed]
 class cartNN(nn.Module):
   def __init__(self, hiddenSize):
     super(cartNN, self).__init__()
-    #define default states
-    #not sure if I'm gonna need these or not, nothing I see online has variables like this in the default constructor
-    #self.cartPosition = 0
-    #self.cartVelocity = 0
-    #self.pendulumAngle = 0
-    #self.angularSpeed = 0
-    #self.force = 0
     self.hiddenLayer1 = nn.Linear(4, hiddenSize)
     self.outputLayer = nn.Linear(hiddenSize, 1)
   def forward(self, f):
-    f = self.flatten(f)
-    logits = self.linear_relu_stack(f)
-    return logits
+    #use ReLU on the first hidden layer
+    f = F.relu(self.hiddenLayer1(x))
+    f = self.outputLayer(f)
+    return f
+  
 
 
 
